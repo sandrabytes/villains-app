8fa3b6329f878553e08ebaee875efc3cf7f98e98
sandrabytes <sandra.guajardo.pena@gmail.com>
2023-02-14 18:15:15 -0600

    added new string

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-document.querySelector('button').addEventListener('click', apiRequest)
+document.querySelector('button').addEventListener('click', apiRequest);
 
-async function apiRequest(){
-  const villainName = document.querySelector('input').value
-  try{
-    const response = await fetch(`https://villains-api-100devs.herokuapp.com/api/villains/${villainName}`)
-    const data = await response.json()
-    console.log(data)
+async function apiRequest() {
+  const villainName = document.querySelector('input').value;
+  try {
+    const response = await fetch(`villains-api-production.up.railway.app`);
+    const data = await response.json();
+    console.log(data);
 
-    document.getElementById('villainName').innerText = data.name
-    document.getElementById('villainMovie').innerText = data.movie
-    document.getElementById('villainHome').innerText = data.home
+    document.getElementById('villainName').innerText = data.name;
+    document.getElementById('villainMovie').innerText = data.movie;
+    document.getElementById('villainHome').innerText = data.home;
 
-    document.getElementById('villainImage').src = data.image
-    document.getElementById('villainCaption').innerText = data.name
-  }catch(error) {
-    console.log(error)
+    document.getElementById('villainImage').src = data.image;
+    document.getElementById('villainCaption').innerText = data.name;
+  } catch (error) {
+    console.log(error);
   }
 }
